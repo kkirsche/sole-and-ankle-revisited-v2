4597e112d83c3d0682f968f67f352da2f73f6fca
Kevin Kirsche <kevin.kirsche@one.verizon.com>
2026-03-19 09:09:22 -0400

    Add breakpoints

--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,15 @@ export const WEIGHTS = {
   medium: 600,
   bold: 800,
 };
+
+export const BREAKPOINTS = {
+	tabletMin: 550,   // pixels
+	laptopMin: 1100,  // pixels
+	desktopMin: 1500, // pixels
+}
+
+export const QUERIES = {
+	'tabletAndUp': `(min-width: ${BREAKPOINTS.tabletMin / 16}/rem)`,
+	'laptopAndUp': `(min-width: ${BREAKPOINTS.laptopMin / 16}/rem)`,
+	'desktopAndUp': `(min-width: ${BREAKPOINTS.desktopMin / 16}/rem)`,
+}
